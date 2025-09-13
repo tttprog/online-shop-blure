e57714eff996d70419cfbb66771ae952e0b197f0
tttprog <alibehzad13791122@gmail.com>
2025-09-13 07:09:32 -0400

    fix bugs

--- app/components/MiniCard.vue
+++ app/components/MiniCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="p-3 bg-white/20 border border-white/20 rounded-2xl">
+    <div class="p-3 bg-white/20 border border-white/20 rounded-2xl" data-aos="fade-up">
       <div class="backdrop-blur-sm flex flex-col justify-between gap-3 items-center text-white">
         <Icon :name="props.item?.icon" size="60" />
         <span class="font-bold">{{ props.item?.title }}</span>
