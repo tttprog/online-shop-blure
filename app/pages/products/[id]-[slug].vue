7ec95eb021a7f2b5a68f86f81b0cf974ec052c7d
tttprog <alibehzad13791122@gmail.com>
2025-09-20 14:27:03 -0400

    add product detail page

---
+++ app/pages/products/[id]-[slug].vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="mt-30 flex flex-col gap-10 container mx-auto">
+    <UBreadcrumb :items="items" :ui="{ link: 'text-white', separatorIcon: 'rotate-180 text-white' }" />
+    <div class="flex flex-col gap-10">
+      <div class="flex flex-col gap-5">
+        <h1 class="text-white font-bold text-2xl">کفش مردانه چرمی تبریز</h1>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { BreadcrumbItem } from '@nuxt/ui';
+
+
+const route = useRoute()
+const items = ref<BreadcrumbItem[]>([
+  {
+    label: 'خانه',
+    to: '/'
+  },
+  {
+    label: 'محصولات',
+    to: '/products'
+  },
+  {
+    label: 'مردانه',
+    to: '/products'
+  },
+  {
+    label: 'کفش',
+    to: '/products'
+  },
+])
+</script>
+
+<style></style>
--- app/pages/products/index.vue
+++ app/pages/products/index.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
-    
+  <div class="mt-30 flex flex-col gap-10 container mx-auto">
+
   </div>
+
 </template>
 
 <script lang="ts" setup>
 
 </script>
 
-<style>
-
-</style>
+<style></style>
